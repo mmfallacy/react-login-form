0af68debdebc645e3c7166c2fcc3395792c582df
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-01-13 23:58:44 +0800

    CHORE: Add react-hook-form FEAT: Implement sign up form functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import {useEffect, useContext} from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {Login, SignUp} from './pages'
-import {APIProvider} from './store'
+import {APIProvider, AuthProvider, AuthContext} from './store'
 import {FirebaseAPI} from './utils'
 
 // Read Firebase Credentials from .env
-const FIREBASE_CONFIG = process.env.REACT_APP_FIREBASE_CONFIG
+const FIREBASE_CONFIG = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
 
 // Initialize an instance for the FirebaseAPI
 const API = new FirebaseAPI(FIREBASE_CONFIG)
@@ -14,20 +15,31 @@ function App() {
 
     <APIProvider instance={API}>
 
-      <Router>
-        <Route exact path="/" component={MockHome}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/signup" component={SignUp}/>
+      <AuthProvider>
 
-        <MockNav />
-      </Router>
-      
+        <Router>
+          <Route exact path="/" component={MockHome}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/signup" component={SignUp}/>
+
+          <MockNav />
+        </Router>
+
+      </AuthProvider>
+
     </APIProvider>
 
   );
 }
 
-const MockHome = () => <div>Home</div>
+const MockHome = () => {
+
+  const user = useContext(AuthContext)
+
+  useEffect(()=> console.log(user), [user])
+
+  return (<div>Home</div>)
+}
 
 const MockNav = () => 
     <nav>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,5 @@
 import {useContext} from 'react'
+import {useForm} from "react-hook-form"
 import {APIContext} from '../store'
 
 export function SignUp(){
@@ -6,14 +7,26 @@ export function SignUp(){
     /** @type {FirebaseAPI} - Firebase API Instance*/
     const API = useContext(APIContext)
 
+    /** 
+     * Form Handler 
+     */
+    const {register, handleSubmit} = useForm()
+    
+    /** @param {FormData} data - Sign Up form data */ 
+    const onSubmit = async (data)=>{
+        console.log(data)
+        const {email, pass} = data
+        API.signUp(email, pass)
+    }   
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <h1>Sign Up</h1>
             <label htmlFor="email">Email: </label>
-            <input name="email" />
+            <input ref={register} name="email" />
             <br/>
             <label htmlFor="pass">Password: </label>
-            <input name="pass" />
+            <input ref={register} type="password" name="pass" />
             <br/>
             <button>Signup</button>
             <br/>
