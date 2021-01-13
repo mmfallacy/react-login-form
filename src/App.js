13532410d6c3fae1b37b7dcf00792ae166233ff9
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-01-13 14:05:26 +0800

    FEAT: Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    </div>
+
+    <Router>
+      <Route exact path="/" component={MockHome}/>
+      <Route exact path="/login" component={MockLogin}/>
+      <Route exact path="/signup" component={MockSignUp}/>
+    </Router>
+
   );
 }
+
+const MockHome = () => <div>Home</div>
+const MockLogin = () => <div>Login</div>
+const MockSignUp = () => <div>Sign Up</div>
+
 
 export default App;
